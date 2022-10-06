dbfeb5b76498b1685b4a3413df927d3640fb54a9
YoungJun Park <youngjun108059@gmail.com>
2022-10-07 04:08:54 +0900

    🎉 Kotlin Practice Repo

---
+++ src/test/kotlin/youngjun/me/practice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package youngjun.me.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
