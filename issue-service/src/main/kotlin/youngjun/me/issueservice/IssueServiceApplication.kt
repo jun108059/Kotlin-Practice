6317e2b72753370458b4abfe2ba9abd8ed456ef9
YoungJun Park <youngjun108059@gmail.com>
2022-10-16 23:56:11 +0900

    🗃️BasedEntity 생성(JpaAuditing 활성화)

--- issue-service/src/main/kotlin/youngjun/me/issueservice/IssueServiceApplication.kt
+++ issue-service/src/main/kotlin/youngjun/me/issueservice/IssueServiceApplication.kt
@@ -2,8 +2,10 @@ package youngjun.me.issueservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class IssueServiceApplication
 
 fun main(args: Array<String>) {
