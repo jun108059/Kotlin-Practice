eb812a3e0206ae2152525fa684709dbf0c1a5423
YoungJun Park <youngjun108059@gmail.com>
2022-10-18 01:12:16 +0900

    👔 이슈 생성 로직 추가

---
+++ issue-service/src/main/kotlin/youngjun/me/issueservice/service/IssueService.kt
@@ -0,0 +1,27 @@
+package youngjun.me.issueservice.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import youngjun.me.issueservice.domain.Issue
+import youngjun.me.issueservice.domain.IssueRepository
+import youngjun.me.issueservice.model.IssueRequest
+import youngjun.me.issueservice.model.IssueResponse
+
+@Service
+class IssueService(
+    private val issueRepository: IssueRepository
+) {
+    @Transactional
+    fun create(userId: Long, request: IssueRequest): IssueResponse {
+
+        val issue = Issue(
+            summary = request.summary,
+            description = request.description,
+            userId = userId, // Token 기반으로 복호화
+            type = request.type,
+            priority = request.priority,
+            status = request.status
+        )
+        return IssueResponse(issueRepository.save(issue))
+    }
+}
