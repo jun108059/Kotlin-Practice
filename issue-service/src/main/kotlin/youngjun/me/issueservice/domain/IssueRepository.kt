3dc936157427efe6cfd0afa34656b050c2695db5
YoungJun Park <youngjun108059@gmail.com>
2022-10-19 00:49:16 +0900

    🗃️이슈 조회 JPA 메서드 선언
    - wiki = https://github.com/jun108059/kotlin-practice/wiki/API-Spec

--- issue-service/src/main/kotlin/youngjun/me/issueservice/domain/IssueRepository.kt
+++ issue-service/src/main/kotlin/youngjun/me/issueservice/domain/IssueRepository.kt
@@ -1,5 +1,8 @@
 package youngjun.me.issueservice.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import youngjun.me.issueservice.domain.enums.IssueStatus
 
-interface IssueRepository: JpaRepository<Issue, Long>
+interface IssueRepository : JpaRepository<Issue, Long> {
+    fun findAllByStatusOrderByCreatedAtDesc(status: IssueStatus): List<Issue>?
+}
