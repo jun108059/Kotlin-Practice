c4902c8848e33195d270eb6eb8ca990b51444353
YoungJun Park <youngjun108059@gmail.com>
2023-04-07 01:08:26 +0900

    📚 baekjoon Main sample 추가

---
+++ src/main/kotlin/algorithm/baekjoon/Main.kt
@@ -0,0 +1,10 @@
+package algorithm.baekjoon
+
+/**
+ * baekjoon input sample
+ */
+fun main(args: Array<String>) {
+    val count = readln().toInt()
+    val num = readln().toInt()
+    println("count: $count, num: $num")
+}
