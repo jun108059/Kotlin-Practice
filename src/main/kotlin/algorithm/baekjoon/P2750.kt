34c09f3e30da6a1af63be1c44d30a1b8b5d7704e
YoungJun Park <youngjun108059@gmail.com>
2023-04-06 23:42:25 +0900

    📚 PS 정렬(baekjoon-2750)
    https://www.acmicpc.net/problem/2750

---
+++ src/main/kotlin/algorithm/baekjoon/P2750.kt
@@ -0,0 +1,26 @@
+package algorithm.baekjoon
+
+/**
+ * baekjoon 2750
+ * https://www.acmicpc.net/problem/2750
+ */
+fun main() {
+    val numArray = intArrayOf(5, 2, 3, 4, 1)
+    val sortedNumbers = bubbleSort(numArray)
+    println(sortedNumbers.joinToString(","))
+}
+
+fun bubbleSort(numArray: IntArray): IntArray {
+    val size = numArray.size
+    var temp: Int
+    for (i in 0 until size) {
+        for (j in 0 until size - i - 1) {
+            if (numArray[j] > numArray[j + 1]) {
+                temp = numArray[j]
+                numArray[j] = numArray[j + 1]
+                numArray[j + 1] = temp
+            }
+        }
+    }
+    return numArray
+}
