eb0aea07df4f96462f1cd92ddcdbcce6aa7cc352
YoungJun Park <youngjun108059@gmail.com>
2023-04-09 01:02:40 +0900

    📚 PS:리스트(평균 구하기)
    - baekjoon 1546
    - _CollectionsJvm.maxOf()
    - _Collections.average()
    
    https://www.acmicpc.net/problem/1546

---
+++ src/main/kotlin/algorithm/baekjoon/P1546.kt
@@ -0,0 +1,25 @@
+package algorithm.baekjoon
+
+/**
+ * 평균 구하기(배열/리스트)
+ *
+ * - baekjoon 1546(https://www.acmicpc.net/problem/1546)
+ */
+fun main() {
+    val scores = "40 80 60"
+    // val scores = "10 20 30"
+    val testScores = scores.split(" ").map { it.toInt() }
+    val result = calculateAverage(testScores)
+    println(result)
+}
+
+fun calculateAverage(scoreList: List<Int>): Double {
+    return scoreList.map { it.toDouble() / maxOf(it) * 100 }.average()
+}
+
+fun p1546(args: Array<String>) {
+    val numberOfSubjects = readln().toInt()
+    val scoreList = readln().split(" ").map { it.toInt() }
+    val maxScore = scoreList.maxOrNull() ?: 1
+    print(scoreList.map { it.toDouble() / maxScore * 100 }.average())
+}
