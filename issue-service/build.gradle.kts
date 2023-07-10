c6d2afb7d6d10f8b72907e670a7654c87e8609ff
YoungJun Park <youngjun108059@gmail.com>
2023-07-10 23:57:27 +0900

    🔧 add jar manifest

--- issue-service/build.gradle.kts
+++ issue-service/build.gradle.kts
@@ -1,5 +1,11 @@
 apply(plugin= "kotlin-jpa")
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "youngjun.me.issueservice.IssueServiceApplication"
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
