af60f600e0a0a2f7306114c530eb3615f871599e
YoungJun Park <youngjun108059@gmail.com>
2023-08-16 23:24:58 +0900

    📚 PS: 단어의 개수
    https://www.acmicpc.net/problem/1152

---
+++ src/main/kotlin/algorithm/baekjoon/P1152.kt
@@ -0,0 +1,11 @@
+package algorithm.baekjoon
+
+/**
+ * 단어의 개수
+ *
+ * - [baekjoon 1152](https://www.acmicpc.net/problem/1152)
+ */
+fun main() {
+    val words = readln().split(" ")
+    println(words.size)
+}
